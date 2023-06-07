37ff83543adfa5c3f07f5d6c71b6a26e48a680b5
Mohit kumar <83944002+rohit-kumar22@users.noreply.github.com>
2023-06-07 21:37:01 +0530

    second

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    base: "/",
-    plugins: [react()],
-  };
-  if (command !== "serve") {
-    config.base = "/country-info/";
-  }
-
-  return config;
+export default defineConfig({
+  plugins: [react()],
+  base: "/country-info/",
 });
